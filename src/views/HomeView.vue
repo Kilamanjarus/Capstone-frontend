8257c5f6dd948479759daa4e1fbbb253a64e2cfe
max <max.lillie95@gmail.com>
2022-08-26 15:20:50 -0500

    added workoutExercises Index and create

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      workoutExercises: {},
+      workoutExercise: {},
+    };
+  },
+  created: function () {
+    this.workoutExerciseIndex();
+  },
+  methods: {
+    workoutExerciseIndex: function () {
+      console.log("Pulling all Exercises added to workouts...")
+      axios.get("http://localhost:3000/workouts").then(response => {
+        console.log(response.data)
+        this.workoutExercises = response.data
+      })
+    },
+    workoutExerciseCreate: function () {
+      console.log("Adding Exercise to Workout...")
+      axios.post("http://localhost:3000/workouts", this.workoutExercise).then(response => {
+        console.log(response.data)
+      })
+    },
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div>
+      <p><b>Exercise ID: </b><input type="text" v-model="workoutExercise.exercise_id"></p>
+      <p><b>Weights: </b><input type="text" v-model="workoutExercise.added_weight"></p>
+      <p><b>Reps: </b><input type="text" v-model="workoutExercise.reps"></p>
+      <p><b>Sets: </b><input type="text" v-model="workoutExercise.sets"></p>
+      <p><b>Exercise ID: </b><button @click="workoutExerciseCreate">Add to Routine!</button></p>
+    </div>
+    <div>
+      <p v-for="currentExercise in workoutExercises">
+        {{ currentExercise }}
+      </p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style>
+</style>
