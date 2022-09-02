39a48111f0379d1fed19916091efbc62d68bfe28
max <max.lillie95@gmail.com>
2022-09-02 13:29:51 -0500

    workout_exercise update/create have been finished

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@ export default {
       message: "Welcome to Vue.js!",
       workoutExercises: {},
       workoutExercise: {},
+      errors: {}
     };
   },
   created: function () {
@@ -15,23 +16,28 @@ export default {
   methods: {
     workoutExerciseIndex: function () {
       console.log("Pulling all Exercises added to workouts...")
-      axios.get("http://localhost:3000/workouts").then(response => {
+      axios.get("http://localhost:3000/routines").then(response => {
         console.log(response.data)
         this.workoutExercises = response.data
       })
     },
     workoutExerciseCreate: function () {
       console.log("Adding Exercise to Workout...")
-      axios.post("http://localhost:3000/workouts", this.workoutExercise).then(response => {
+      axios.post("http://localhost:3000/routines", this.workoutExercise).then(response => {
         console.log(response.data)
+        this.workoutExercises.push(response.data)
       })
     },
     workoutExerciseUpdate: function (currentExercise) {
       console.log('Updating Exercise...')
       console.log(currentExercise)
-      axios.patch(`http://localhost:3000/workouts/${currentExercise.id}`, currentExercise).then(response => {
+      axios.patch(`http://localhost:3000/routines/${currentExercise.id}`, currentExercise).then(response => {
         console.log(response.data)
       })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+          console.log(this.errors)
+        });
     }
   },
 };
@@ -50,12 +56,15 @@ export default {
     <p><b>Current Workout:</b></p>
     <div>
       <div v-for="currentExercise in workoutExercises">
-        <p>Exercise ID: <input type="text" v-model="currentExercise.exercise_id"></p>
-        <p>Weight: <input type="text" v-model="currentExercise.added_weight"></p>
-        <p>Sets: <input type="text" v-model="currentExercise.sets"></p>
-        <p>Reps: <input type="text" v-model="currentExercise.reps"></p>
-        <button @click="workoutExerciseUpdate(currentExercise)">Update Exorcise</button>
-        <br>
+        <div v-if="currentExercise.status === `added`">
+          {{ currentExercise.id }}
+          <p>Exercise ID: <input type="text" v-model="currentExercise.exercise_id"></p>
+          <p>Weight: <input type="text" v-model="currentExercise.added_weight"></p>
+          <p>Sets: <input type="text" v-model="currentExercise.sets"></p>
+          <p>Reps: <input type="text" v-model="currentExercise.reps"></p>
+          <button @click="workoutExerciseUpdate(currentExercise)">Update Exorcise</button>
+          <br>
+        </div>
       </div>
     </div>
   </div>
