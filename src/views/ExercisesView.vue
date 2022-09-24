c1eba609b8087bb6bca0ce30dd0a454e9aa1ed42
max <max.lillie95@gmail.com>
2022-09-24 14:41:26 -0500

    added dropdown to filter by muscle group

--- src/views/ExercisesView.vue
+++ src/views/ExercisesView.vue
@@ -19,6 +19,7 @@ export default {
       exercisesOnPage: [],
 
       searchWords: "",
+      muscleGroup: "",
     };
   },
   created: function () {
@@ -29,29 +30,23 @@ export default {
     exercisesIndex: function () {
       console.log(`Retrieving exercises...`)
       axios.get("http://localhost:3000/exercises.json").then(response => {
-        console.log(response.data)
+        // console.log(response.data)
         this.exercises = response.data
         this.updateExercisesOnPage();
-        if (this.exercises.length / this.exercisesPerPage % 1 > 0) {
-          this.exercisePageAmount = (this.exercises.length / this.exercisesPerPage + 1) - (this.exercises.length / this.exercisesPerPage % 1)
-        }
-        else {
-          this.exercisePageAmount = this.exercises.length / this.exercisesPerPage
-        }
         // console.log(this.exercisePageAmount)
         // console.log(localStorage.jwt)
       })
     },
     openOptions: function (exercise) {
       console.log(`Opening up options to add a routine...`)
-      console.log(exercise)
+      // console.log(exercise)
       this.newRoutine = {}
       this.currentExercise = exercise
       document.querySelector('#routine-details').showModal();
     },
     routineCreate: function (exercise) {
       console.log(`Creating new routine....`)
-      console.log(exercise)
+      // console.log(exercise)
       this.newRoutine.exercise_id = exercise.id
       if (exercise.equipment == "body weight" || exercise.equipment == "assisted") {
         this.newRoutine.added_weight = 0
@@ -67,14 +62,14 @@ export default {
       console.log(page)
       this.pageNumber = page
       this.exerciseIndex = page - 1
-      console.log(this.pageNumber)
+      // console.log(this.pageNumber)
       this.updateExercisesOnPage(page);
       window.scrollTo(0, 0);
     },
     setPrevPageNumber: function () {
       this.pageNumber = this.pageNumber - 1
       this.exerciseIndex = this.pageNumber - 1
-      console.log(this.pageNumber)
+      // console.log(this.pageNumber)
       this.updateExercisesOnPage(this.pageNumber);
       window.scrollTo(0, 0);
     },
@@ -87,8 +82,8 @@ export default {
       // console.log(this.pageNumber)
     },
     updateExercisesOnPage: function (page) {
-      console.log("New array is being made...")
-      console.log(page)
+      // console.log("New array is being made...")
+      // console.log(page)
       this.exercisesOnPage = []
       for (let i = 0; i < this.exercisesPerPage; i++) {
         if (this.filterExercises()[i + (this.exerciseIndex * this.exercisesPerPage)]) {
@@ -96,19 +91,25 @@ export default {
         }
       }
       // console.log(this.exercises[0])
-      console.log(this.exercisesOnPage)
+      // console.log(this.exercisesOnPage)
+      if (this.exercises.length / this.exercisesPerPage % 1 > 0) {
+        this.exercisePageAmount = (this.exercises.length / this.exercisesPerPage + 1) - (this.exercises.length / this.exercisesPerPage % 1)
+      }
+      else {
+        this.exercisePageAmount = this.exercises.length / this.exercisesPerPage
+      }
+      // console.log(this.exercises)
     },
     filterExercises: function () {
       // console.log('Filtering exercises...')
       return this.exercises.filter(exercise => {
-        return exercise.name.includes(this.searchWords);
+        return exercise.name.includes(this.searchWords) && exercise.target.includes(this.muscleGroup)
       })
     },
     filterMuscles: function (word) {
-      console.log('Filtering Muscles...')
-      return this.exercises.filter(exercise => {
-        return exercise.target.includes(word);
-      })
+      this.muscleGroup = word
+      console.log(word)
+      this.updateExercisesOnPage();
     },
   },
 };
@@ -119,6 +120,36 @@ export default {
     <h1>{{ message }}</h1>
 
     <p>Search Here:</p>
+
+    <span class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+        aria-haspopup="true" aria-expanded="false">
+        Muscle Group
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" @click="filterMuscles('')">All</a>
+        <a class="dropdown-item" @click="filterMuscles('adductors')">Adductors</a>
+        <a class="dropdown-item" @click="filterMuscles('abductors')">Adductors</a>
+        <a class="dropdown-item" @click="filterMuscles('abs')">Abs</a>
+        <a class="dropdown-item" @click="filterMuscles('biceps')">Biceps</a>
+        <a class="dropdown-item" @click="filterMuscles('calves')">Calves</a>
+        <a class="dropdown-item" @click="filterMuscles('cardiovascular system')">Cardio</a>
+        <a class="dropdown-item" @click="filterMuscles('delts')">Delts</a>
+        <a class="dropdown-item" @click="filterMuscles('forearms')">Forearms</a>
+        <a class="dropdown-item" @click="filterMuscles('glutes')">Glutes</a>
+        <a class="dropdown-item" @click="filterMuscles('Hamstrings')">Hamstrings</a>
+        <a class="dropdown-item" @click="filterMuscles('levator scapulae')">Levator Scapulae</a>
+        <a class="dropdown-item" @click="filterMuscles('pectorals')">Pectorals</a>
+        <a class="dropdown-item" @click="filterMuscles('quads')">Quads</a>
+        <a class="dropdown-item" @click="filterMuscles('serratus anterior')">Serratus Anterior</a>
+        <a class="dropdown-item" @click="filterMuscles('spine')">Spine</a>
+        <a class="dropdown-item" @click="filterMuscles('lats')">Lats</a>
+        <a class="dropdown-item" @click="filterMuscles('traps')">Traps</a>
+        <a class="dropdown-item" @click="filterMuscles('triceps')">Triceps</a>
+        <a class="dropdown-item" @click="filterMuscles('upper back')">Upper Back</a>
+      </div>
+    </span>
+
     <input type="text" v-model="searchWords">
     <button @click="updateExercisesOnPage()">Workout Title Search...</button>
     <p></p>
